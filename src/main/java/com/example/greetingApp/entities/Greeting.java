29d723102ab99bd92fbd2827ec3536e3346bd3d5
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-02-23 11:49:47 +0530

    [Aakrati] add. UC3 done

---
+++ src/main/java/com/example/greetingApp/entities/Greeting.java
@@ -0,0 +1,39 @@
+package com.example.greetingApp.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Greeting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String message;
+
+    public Greeting() {}
+
+    // Constructor
+    public Greeting(String message) {
+        this.message = message;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/com/example/greetingApp/services/GreetingServices.java
+++ src/main/java/com/example/greetingApp/services/GreetingServices.java
@@ -1,9 +1,30 @@
 package com.example.greetingApp.services;
 
+import com.example.greetingApp.entities.Greeting;
+import com.example.greetingApp.repository.GreetingRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GreetingServices {
+
+    private final GreetingRepository greetingRepository;
+
+    GreetingServices(GreetingRepository greetingRepository){
+        this.greetingRepository = greetingRepository;
+    }
+
+    // Save greeting message
+    public Greeting saveGreeting(String message) {
+        Greeting greeting = new Greeting(message);
+        return greetingRepository.save(greeting);
+    }
+
+    // Get all saved greetings
+    public List<Greeting> getAllGreetings() {
+        return greetingRepository.findAll();
+    }
 
     public String getMessage(){
         return "Hello world!";
